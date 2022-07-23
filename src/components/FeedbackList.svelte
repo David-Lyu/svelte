481ace1bc884bb6b7743c20cec5be4826ac5756b
david <lyu.david@yahoo.com>
2022-07-22 19:57:57 -0700

    Redo Svelete

--- src/components/FeedbackList.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import FeedbackItem from "./FeedbackItem.svelte";
-
-  export let feedback = [];
-</script>
-
-{#each feedback as feedItem (feedItem.id)}
-  <FeedbackItem {feedItem} on:delete-feedback />
-{/each}

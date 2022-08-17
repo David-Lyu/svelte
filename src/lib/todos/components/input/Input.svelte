efee877ed6e71548698ee2709b1bd3ea8e078749
DavidLyu <59672932+David-Lyu@users.noreply.github.com>
2022-08-16 17:59:39 -0700

    Got input to work and update the store

--- src/lib/todos/components/input/Input.svelte
+++ src/lib/todos/components/input/Input.svelte
@@ -1,20 +1,31 @@
-<script>
-	let todo = {};
+<script lang="ts">
+	import { todosStore } from '../../../utils/store/store.ts';
+	import type { todo } from '$lib/todos/types/todo.d.ts';
+	let userInput = {
+		header: ''
+	};
 
-	onSubmit = (e, value) => {
+	const onSubmit = (e, value) => {
 		e.preventDefault();
-		console.log('submited');
+		console.log(userInput);
+		todosStore.update((todos) => {
+			console.log(todos);
+			userInput = {
+				header: ''
+			};
+			return [...todos, userInput];
+		});
 	};
 </script>
 
 <h4>Add A Todo:</h4>
-<form>
-	<label>Todo</label>
-	<input />
-	<label>Description</label>
-	<input />
-	<label>Date</label>
-	<input />
+<form on:submit={onSubmit}>
+	<label name="todo">Todo</label>
+	<input id="todo" bind:value={userInput.header} />
+	<label name="description">Description</label>
+	<input id="description" bind:value={userInput.description} />
+	<label name="date">Date</label>
+	<input id="date" bind:value={userInput.date} />
 
 	<input type="submit" />
 </form>
--- src/lib/utils/store/store.ts
+++ src/lib/utils/store/store.ts
@@ -1,6 +1,5 @@
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
-import type Todos from '$lib/todos/components/todos/Todos.svelte';
 
-export const todos: Writable<Todos[]> = writable([]);
-export const archivedTodos: Writable<todo[]> = writable([]);
+export const todosStore: Writable<todo[]> = writable([]);
+export const archivedTodosStore: Writable<todo[]> = writable([]);

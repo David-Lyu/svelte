bff33bbffb35d354a18fd4b6a671ca6bbd18b465
DavidLyu <59672932+David-Lyu@users.noreply.github.com>
2022-08-16 17:20:53 -0700

    Started working on todos

--- src/lib/todos/components/input/Input.svelte
+++ src/lib/todos/components/input/Input.svelte
@@ -1,3 +1,20 @@
-<script></script>
+<script>
+	let todo = {};
 
-Inside scripts
+	onSubmit = (e, value) => {
+		e.preventDefault();
+		console.log('submited');
+	};
+</script>
+
+<h4>Add A Todo:</h4>
+<form>
+	<label>Todo</label>
+	<input />
+	<label>Description</label>
+	<input />
+	<label>Date</label>
+	<input />
+
+	<input type="submit" />
+</form>

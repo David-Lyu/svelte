549cfaf676124c02ee49ed2208055f7de97830b8
DavidLyu <59672932+David-Lyu@users.noreply.github.com>
2022-08-17 18:34:26 -0700

    Added some faulty validation to input

--- src/lib/todos/components/input/Input.svelte
+++ src/lib/todos/components/input/Input.svelte
@@ -5,10 +5,26 @@
 		header: ''
 	};
 
+	let isHeaderTouched = false;
+	let isHeaderValid = false;
+
+	const onKeyDown = (e) => {
+		console.log(e);
+		if (e.currentTarget.value.length > 0) {
+			isHeaderTouched = true;
+		}
+	};
+
 	const onSubmit = (e, value) => {
 		e.preventDefault();
+		if (!userInput.header) {
+			//should alert user that this field cannot be blank
+			isHeaderValid = false;
+			return;
+		}
 		console.log(userInput);
 		todosStore.update((todos) => {
+			isHeaderTouched = false;
 			console.log(todos);
 			userInput = {
 				header: ''
@@ -21,7 +37,12 @@
 <h4>Add A Todo:</h4>
 <form on:submit={onSubmit}>
 	<label name="todo">Todo</label>
-	<input id="todo" bind:value={userInput.header} />
+	<div>
+		<input id="todo" bind:value={userInput.header} on:keydown={onKeyDown} />
+		{#if isHeaderTouched && !isHeaderValid}
+			<p>This field cannot be blank</p>
+		{/if}
+	</div>
 	<label name="description">Description</label>
 	<input id="description" bind:value={userInput.description} />
 	<label name="date">Date</label>
